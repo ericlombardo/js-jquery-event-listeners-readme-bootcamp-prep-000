47ae828f2ec0cd73c25b3e503d18a3cebcc753e3
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-03 19:37:34 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -8,7 +8,7 @@ function getIt() {
 //
 function frameIt() {
   $('img').on('load', function() {
-    console.log($('img[0]').attributes);
+    console.log($('img[0]').className);
   });
 }
 $(document).ready(function(){
