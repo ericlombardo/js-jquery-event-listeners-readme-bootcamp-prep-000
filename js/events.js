e6eed4143c5b7f0c4991967061d6a4e6a221585b
sincere-trainer-5337 <eric.m.lombardo@gmail.com>
2020-09-03 19:19:04 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,9 +1,15 @@
 //define functions here
-//
+// event handler on paragraphs that alert message when clicked
 function getIt() {
   $('p').on('click', function() {
     alert('hey!');
   });
+}
+//
+function frameIt() {
+  $('img').on('load', function() {
+    img.style.border += 'tasty';
+  })
 }
 $(document).ready(function(){
 
